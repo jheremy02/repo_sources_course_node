cc8b11ea420a4cbb1832bd222571789335ab6985
Jheyson Menéndez <jheremy26@gmail.com>
2022-03-08 11:58:47 -0500

    practica sobre hoisting: el consiste en que javascript coloca las declaraciones de variable y funciones en las primeras lineas del codigo

---
+++ examples/hoisting.js
@@ -0,0 +1,36 @@
+let x=100;
+
+let y=function () {
+    let x ;
+
+    if (x!=20) {
+        let x = 30;
+        return x;
+    }
+
+    
+}
+
+console.log(x,y())
+
+console.log("================")
+
+
+console.log(myName)
+
+
+var myName="Jheyson";
+
+
+console.log(myName)
+
+
+console.log("================")
+
+console.log(hey());
+
+function hey() {
+    return '"""""'
+}
+
+var cadena="hello world"
